aab4755127f0a40195aed3bd210fcc2ec27736a2
gunderjt <gunderjt@colorado.edu>
2013-09-11 17:54:17 -0700

    Initial commit

---
+++ lab2/Lab2.rb
@@ -0,0 +1,110 @@
+# Part1: Hello World
+class HelloWorldClass
+    def initialize(name)
+       @name = name.capitalize
+    end
+    def sayHi
+        puts "Hello #{@name}!"
+    end
+end
+
+def palindrome?(string)
+  # your code here
+  string = string.gsub(/[^a-zA-Z]/, '')
+  string = string.downcase
+  if (string === string.reverse)
+    puts "true"
+  else
+    puts "false"
+  end
+end
+
+def count_words(string)
+  # your code here
+  string = string.gsub(/[^a-zA-Z ]/, '')
+  string = string.downcase
+  words = string.split
+  hashWord = Hash.new
+  words.each {|word|
+    if(hashWord.has_key?(word))
+      hashWord[word] = array[word] + 1
+    else
+      hashWord[word] = 1
+    end
+  }
+  puts hashWord.inspect
+end
+
+class WrongNumberOfPlayersError <  StandardError ; end
+class NoSuchStrategyError <  StandardError ; end
+
+def validStrategy?(strategy)
+  strategy = strategy.downcase
+  valid = ["r", "p", "s"]
+  if(valid.include?(strategy))
+    return true
+  else
+    return false
+  end
+end
+
+def secondPlayerWin?(firstPlayer, secondPlayer)
+  firstPlayer = firstPlayer.downcase
+  secondPlayer = secondPlayer.downcase
+  if(firstPlayer == "r" && secondPlayer == "p")
+    return true
+  elsif (firstPlayer == "p" && secondPlayer == "s")
+    return true
+  elsif (firstPlayer == "s" && secondPlayer == "r")
+    return true
+  else
+    return false
+  end
+end
+
+def rps_game_winner(game)
+  raise WrongNumberOfPlayersError unless game.length == 2
+  raise NoSuchStrategyError unless validStrategy?(game[0][1]) && validStrategy?(game[1][1])
+  if(secondPlayerWin?(game[0][1], game[1][1]))
+    return game[1]
+  else
+    return game[0]
+  end
+end
+
+bracket = [
+           [
+            [["Armando", "P"], ["Dave", "S"]],
+            [["Richard", "R"], ["Michael", "S"]],
+           ],
+           [
+            [["Allen", "S"], ["Omer", "P"]],
+            [["David E.", "R"], ["Richard X.", "P"]]
+           ]
+          ]
+
+
+#puts rps_tournament_winner(bracket)
+
+def rps_tournament_winner(bracket)
+  if (bracket[0][0].is_a?(String))
+    return rps_game_winner(bracket)
+  end
+  newBracket = Array.new
+  newBracket << rps_tournament_winner(bracket[0])
+  newBracket << rps_tournament_winner(bracket[1])
+  return rps_game_winner(newBracket)
+end
+
+#puts rps_game_winner([ ["Armando", "S"], ["Dave", "R"] ])
+
+count_words("A man, a plan, a canal -- Panama")
+count_words("doo bee doo bee doo")
+
+
+#hello = HelloWorldClass.new("jeffrey gunderson")
+#hello.sayHi
+
+#palindrome?("A man, a plan, a canal -- Panama")
+#palindrome?("Madam, I'm Adam!")
+#palindrome?("Abracadabra")
