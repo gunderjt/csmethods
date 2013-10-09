b30e99aa6a16fd92193f84136ccb04aead7aaf86
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2013-10-08 21:20:54 -0700

    Maybe done with part 3, still need to test: Goodnight

--- hw2_rottenpotatoes/app/controllers/movies_controller.rb
+++ hw2_rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,20 +7,40 @@ class MoviesController < ApplicationController
   end
 
   def index
-    
-    @all_ratings = Movie.select("DISTINCT rating").map(&:rating)
-    # @movies = Movie.all
+    #debugger
+    @all_ratings = Movie.select("DISTINCT rating").map {|i| i.rating}
+
     searchArgs = Hash.new
+    redirectArgs = Hash.new
     order = ""
+    no_redirect = 0
+
     # if sessions exists and params doesn't have keys :ratings or :sort
-    # AND params :ratings exists but doesn't have any keys, redirect_to the 
+    # AND params :ratings exists but doesn't have any keys, redirect_to the
     # movie_link_path(:ratings => session[:ratings], :sort => session[:sort])
     # don't forget your flash.keep before redirect
-    if params.has_key?(:ratings)
+    #    if params.has_key?(:ratings)
+    if params[:ratings]
       showRatings = params[:ratings].keys
       searchArgs[:conditions] = [" rating in (?) ", showRatings]
+      session[:ratings] = params[:ratings]
+      session.delete(:sort)
+      no_redirect = 1
     end
-    searchArgs[:order] = params[:sort] if params.has_key?(:sort)
+    #    if params.has_key?(:sort)
+    if params[:sort]
+#      debugger
+      searchArgs[:order] = params[:sort]
+      session[:sort] = params[:sort]
+      unless params.has_key?(:ratings)
+        session.delete(:ratings)
+      end
+      no_redirect = 1
+    end
+    if (no_redirect == 0 && (session.has_key?(:sort) || session.has_key?(:ratings)))
+      flash.keep
+      redirect_to movie_path(:sort => session[:sort], :ratings => session[:ratings])
+    end
     @movies = Movie.find(:all, searchArgs)
     @titleClass = "hilite" if params[:sort] == "title"
     @dateClass = "hilite" if params[:sort] == "release_date"
